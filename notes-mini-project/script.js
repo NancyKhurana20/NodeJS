71ba98d16a597612e6dfe0bf6ab333e9668c3160
Nancy Khurana <khurananancy36@gmail.com>
2026-09-11 23:30:21 +0530

    Add task file viewing

--- notes-mini-project/script.js
+++ notes-mini-project/script.js
@@ -25,6 +25,15 @@ app.post("/create", function (req, res) {
     },
   );
 });
+app.get("/file/:filename", function (req, res) {
+  fs.readFile(
+    `./files/${req.params.filename}`,
+    "utf-8",
+    function (err, filedata) {
+      res.render("show", { filename: req.params.filename, filedata: filedata });
+    },
+  );
+});
 app.listen("3001", () => {
   console.log("Server is running on http://localhost:3001");
 });
