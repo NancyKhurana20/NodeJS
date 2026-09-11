0837cb3f660426e3d62fb51315d3d61a4568b997
Nancy Khurana <khurananancy36@gmail.com>
2026-09-11 22:41:39 +0530

    Add task creation form and POST route

--- notes-mini-project/script.js
+++ notes-mini-project/script.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -8,9 +9,16 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
-  res.render("index");
+  //Reading all the files and sending their names to EJS page
+  //fs.readdir() is a Node.js File System method used to read the contents of a directory.
+  fs.readdir(path.join(__dirname, "files"), function (err, files) {
+    res.render("index", { files: files }); //Render the index.ejs file and send the files data to it.
+    //if there will be no file initially inside the files folder , it will send an empty array to the index.ejs
+  });
 });
-
+app.post("/create", function (req, res) {
+  console.log(req.body);
+});
 app.listen("3001", () => {
   console.log("Server is running on http://localhost:3001");
 });
