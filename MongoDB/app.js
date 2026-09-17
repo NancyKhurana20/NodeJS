c9dce594b82e10f571641ff1ed3cf9b63a6ffd89
Nancy Khurana <khurananancy36@gmail.com>
2026-09-17 20:05:31 +0530

    Learn MongoDB CRUD operations with Mongoose

--- MongoDB/app.js
+++ MongoDB/app.js
@@ -1,10 +1,39 @@
 const express = require("express");
 const app = express();
 
+const userModel = require("./usermodel");
+
 app.get("/", function (req, res) {
   res.send("hey");
 });
 
+app.get("/create", async (req, res) => {
+  let createdUser = await userModel.create({
+    name: "Nancy",
+    email: "nancy@gmail.com",
+    username: "nancy",
+  });
+  res.send(createdUser);
+});
+
+app.get("/update", async (req, res) => {
+  let user = await userModel.findOneAndUpdate(
+    { username: "nancy" },
+    { name: "Nancy Khurana" },
+    { returnDocument: "after" },
+  );
+  res.send(user);
+});
+
+app.get("/read", async (req, res) => {
+  let users = await userModel.find({ username: "nancy" });
+  res.send(users);
+});
+
+app.get("/delete", async (req, res) => {
+  let users = await userModel.findOneAndDelete({ username: "nancy" });
+  res.send(users);
+});
 app.listen("3002", function () {
   console.log("Server running on http://localhost:3002");
 });
