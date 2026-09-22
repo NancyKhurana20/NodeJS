74043e174f5fe31bd84f1dd9957c4f485ef3b21f
Nancy Khurana <khurananancy36@gmail.com>
2026-09-22 21:00:33 +0530

    Add user registration validation

--- Mini-Project/app.js
+++ Mini-Project/app.js
@@ -11,6 +11,13 @@ app.get("/", function (req, res) {
   res.render("index");
 });
 
+app.post("/register", async function (req, res) {
+  let { username, name, email, password, age } = req.body;
+
+  let user = await userModel.findOne({ email });
+  if (user) return res.status(500).send("User already registered");
+});
+
 app.listen("3007", function () {
   console.log("Server running on http://localhost:3007");
 });
