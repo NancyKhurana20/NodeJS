9f7714b9c40e29ca9acfb7609483c5f3e0df4d54
Nancy Khurana <khurananancy36@gmail.com>
2026-09-22 21:26:31 +0530

    Implement user registration and post associations

--- Mini-Project/app.js
+++ Mini-Project/app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const userModel = require("./models/user");
+const postModel = require("./models/post");
+const ejs = require("ejs");
 
-app.set("view engine", ejs);
+app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -16,6 +21,22 @@ app.post("/register", async function (req, res) {
 
   let user = await userModel.findOne({ email });
   if (user) return res.status(500).send("User already registered");
+
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      let user = await userModel.create({
+        username,
+        email,
+        age,
+        name,
+        password: hash,
+      });
+
+      let token = jwt.sign({ email: email, userid: user._id }, "shhhh");
+      res.cookie("token", token);
+      res.send("registered");
+    });
+  });
 });
 
 app.listen("3007", function () {
