4972ebd7ac06dc1ac307c7665cad90a3c4c41f77
Nancy Khurana <khurananancy36@gmail.com>
2026-09-24 19:52:55 +0530

    Add image upload functionality with Multer

--- Mini-Project/app.js
+++ Mini-Project/app.js
@@ -7,10 +7,29 @@ const userModel = require("./models/user");
 const postModel = require("./models/post");
 const ejs = require("ejs");
 
+const multer = require("multer");
+
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+const crypto = require("crypto");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/images/uploads");
+  },
+  filename: function (req, file, cb) {
+    crypto.randomBytes(12, function (err, bytes) {
+      const fn = bytes.toString("hex") + path.extname(file.originalname);
+      cb(null, fn);
+    });
+  },
+});
+
+const upload = multer({ storage: storage });
 
 app.get("/", function (req, res) {
   res.render("index");
@@ -41,6 +60,13 @@ app.post("/register", async function (req, res) {
       res.send("registered");
     });
   });
+});
+
+app.get("/test", (req, res) => {
+  res.render("test");
+});
+app.post("/upload", upload.single("image"), (req, res) => {
+  console.log(req.file);
 });
 
 app.post("/login", async function (req, res) {
