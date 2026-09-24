b6e1da093d2e670d05fdc312aaadf5b1dd9822fd
Nancy Khurana <khurananancy36@gmail.com>
2026-09-24 18:45:55 +0530

    Implement post edit functionality

--- Mini-Project/app.js
+++ Mini-Project/app.js
@@ -70,6 +70,19 @@ app.use("/profile", isLoggedIn, async function (req, res) {
   res.render("profile", { user });
 });
 
+app.use("/edit/:id", isLoggedIn, async function (req, res) {
+  let post = await postModel.findOne({ _id: req.params.id }).populate("user");
+  res.render("edit", { post });
+});
+
+app.post("/update/:id", isLoggedIn, async function (req, res) {
+  let post = await postModel.findOneAndUpdate(
+    { _id: req.params.id },
+    { content: req.body.content },
+  );
+  res.redirect("/profile");
+});
+
 app.use("/like/:id", isLoggedIn, async function (req, res) {
   let post = await postModel.findOne({ _id: req.params.id }).populate("user");
 
