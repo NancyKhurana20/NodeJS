f45cc4b3b105675d7629f4558a62ec89b852e8a2
Nancy Khurana <khurananancy36@gmail.com>
2026-09-24 18:29:24 +0530

    Implement post like and unlike functionality

--- Mini-Project/app.js
+++ Mini-Project/app.js
@@ -70,6 +70,19 @@ app.use("/profile", isLoggedIn, async function (req, res) {
   res.render("profile", { user });
 });
 
+app.use("/like/:id", isLoggedIn, async function (req, res) {
+  let post = await postModel.findOne({ _id: req.params.id }).populate("user");
+
+  if (post.likes.indexOf(req.user.userid) === -1) {
+    post.likes.push(req.user.userid);
+  } else {
+    post.likes.splice(post.likes.indexOf(req.user.userid), 1);
+  }
+
+  await post.save();
+  res.redirect("/profile");
+});
+
 app.use("/post", isLoggedIn, async function (req, res) {
   let user = await userModel.findOne({ email: req.user.email });
   let { content } = req.body;
