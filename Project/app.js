6185bf09c5c98e20d7d7e7a1e5dcbcb34e492731
Nancy Khurana <khurananancy36@gmail.com>
2026-09-25 17:29:04 +0530

    Add user owner and product routers

--- Project/app.js
+++ Project/app.js
@@ -8,11 +8,19 @@ const jwt = require("jsonwebtoken");
 
 const db = require("./config/mongoose-connection");
 
+const ownersRouter = require("./routes/ownersRouter");
+const usersRouter = require("./routes/usersRouter");
+const productsRouter = require("./routes/productsRouter");
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
+
+app.use("/users", usersRouter);
+app.use("/owners", ownersRouter);
+app.use("/products", productsRouter);
 
 app.listen("3009", function () {
   console.log("Server running on http://localhost:3007");
